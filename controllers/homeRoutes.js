0d02379f7489c1b80e01af7c3f36057be11b4582
Mustafa Ahmed <musi1996@yahoo.com>
2024-08-24 14:41:16 -0700

    initial routes and api routes setup

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const { Post, User } = require('../models');
+
+// GET / - Homepage
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [{ model: User, attributes: ['username'] }],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('homepage', {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET /login
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('login');
+});
+
+// GET /signup
+router.get('/signup', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('signup');
+});
+
+module.exports = router;
