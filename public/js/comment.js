8316343b0b92f08b35adfc2b0bd9c2101c13423f
Mustafa Ahmed <musi1996@yahoo.com>
2024-08-24 20:14:14 -0700

    initial public and views with handlebars/functionality for each page in js

---
+++ public/js/comment.js
@@ -0,0 +1,22 @@
+const commentFormHandler = async (event) => {
+    event.preventDefault();
+
+    const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
+    const post_id = window.location.toString().split('/').pop();
+
+    if (comment_text) {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({ comment_text, post_id }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert('Failed to add comment.');
+        }
+    }
+};
+
+document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
---
+++ public/js/editPost.js
@@ -0,0 +1,23 @@
+const editFormHandler = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value.trim();
+    const content = document.querySelector('textarea[name="post-content"]').value.trim();
+    const id = window.location.toString().split('/').pop();
+
+    if (title && content) {
+        const response = await fetch(`/api/posts/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({ title, content }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to update post.');
+        }
+    }
+};
+
+document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
