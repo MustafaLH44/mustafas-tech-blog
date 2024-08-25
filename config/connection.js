32ebff96aa596622ada3ea4df4a8f72708e9fe1e
Mustafa Ahmed <musi1996@yahoo.com>
2024-08-25 11:12:17 -0700

    made changes to .env because of errors

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,37 @@
+// const Sequelize = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: 'postgres',
+//     port: process.env.DB_PORT
+//   }
+// );
+
+// module.exports = sequelize;
+
 const Sequelize = require('sequelize');
-require('dotenv').config();
+require('dotenv').config('../.env');
+let sequelize;
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    port: process.env.DB_PORT
-  }
-);
-
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'postgres'
+    }
+  );
+}
 module.exports = sequelize;
